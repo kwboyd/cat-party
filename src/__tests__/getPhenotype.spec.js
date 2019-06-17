ea9c44291f26dc07099c3d821efe493e4cca2b5c
Kate Boyd <kwboyd95@gmail.com>
2019-06-16 21:06:36 -0600

    Refactor phenotype testing

--- src/__tests__/getPhenotype.spec.js
+++ src/__tests__/getPhenotype.spec.js
@@ -3,49 +3,39 @@ import { geneData } from '../catData/genes';
 
 // we want to make sure that all combinations of phenotypes are valid
 
-describe('whiskers genes', () => {
-    test('all gene combinations have a phenotype', () => {
-        const whiskerGenes = Object.keys(geneData.whiskers);
-        for (let i = 0; i < whiskerGenes.length; i++) {
-            const primaryGene = whiskerGenes[i];
-            for (let j = i; j < whiskerGenes.length; j++) {
-                const secondaryGene = whiskerGenes[j];
-                console.log(`testing for ${primaryGene}${secondaryGene} in whiskers`);
-                const phenotype = getPhenotype([primaryGene, secondaryGene], 'whiskers');
-                expect(phenotype.left).toBeTruthy();
-                expect(phenotype.right).toBeTruthy();
-            }
+function getListOfPhenotypes(geneType) {
+    const phenotypes = [];
+    const genes = Object.keys(geneData[geneType]);
+    for (let i = 0; i < genes.length; i++) {
+        const primaryGene = genes[i];
+        for (let j = i; j < genes.length; j++) {
+            const secondaryGene = genes[j];
+            console.log(`testing for ${primaryGene}${secondaryGene} in ${geneType}`);
+            const phenotype = getPhenotype([primaryGene, secondaryGene], geneType);
         }
+    }
+    return phenotypes;
+}
+
+test('all whisker gene combinations have a phenotype', () => {
+    const phenotypes = getListOfPhenotypes('whiskers');
+    phenotypes.forEach(phenotype => {
+        expect(phenotype.left).toBeTruthy();
+        expect(phenotype.right).toBeTruthy();
     })
 })
 
-describe('eyes genes', () => {
-    test('all gene combinations have a phenotype', () => {
-        const eyeGenes = Object.keys(geneData.eyes);
-        for (let i = 0; i < eyeGenes.length; i++) {
-            const primaryGene = eyeGenes[i];
-            for (let j = i; j < eyeGenes.length; j++) {
-                const secondaryGene = eyeGenes[j];
-                console.log(`testing for ${primaryGene}${secondaryGene} in eyes`);
-                const phenotype = getPhenotype([primaryGene, secondaryGene], 'eyes');
-                expect(phenotype.left).toBeTruthy();
-                expect(phenotype.right).toBeTruthy();
-            }
-        }
+test('all eye gene combinations have a phenotype', () => {
+    const phenotypes = getListOfPhenotypes('eyes');
+    phenotypes.forEach(phenotype => {
+        expect(phenotype.left).toBeTruthy();
+        expect(phenotype.right).toBeTruthy();
     })
 })
 
-describe('nose genes', () => {
-    test('all gene combinations have a phenotype', () => {
-        const noseGenes = Object.keys(geneData.nose);
-        for (let i = 0; i < noseGenes.length; i++) {
-            const primaryGene = noseGenes[i];
-            for (let j = i; j < noseGenes.length; j++) {
-                const secondaryGene = noseGenes[j];
-                console.log(`testing for ${primaryGene}${secondaryGene} in nose`);
-                const phenotype = getPhenotype([primaryGene, secondaryGene], 'nose');
-                expect(phenotype.center).toBeTruthy();
-            }
-        }
+test('all nose gene combinations have a phenotype', () => {
+    const phenotypes = getListOfPhenotypes('nose');
+    phenotypes.forEach(phenotype => {
+        expect(phenotype.center).toBeTruthy();
     })
 })
